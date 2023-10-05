656a41d0fe9cd69111a352d93f1b718ec0d9f339
CUMT-Atom <cnatom@foxmail.com>
2023-10-05 16:52:48 +0800

    Initial Commit

---
+++ FoodPicker/FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by atom on 2023/10/5.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
