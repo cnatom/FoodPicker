fa9e29f2e949324b93ca80bec2944f5cc60b8c9d
CUMT-Atom <cnatom@foxmail.com>
2023-10-05 18:23:58 +0800

    1_3_第一个画面

--- FoodPicker/ContentView.swift
+++ FoodPicker/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    let food = ["汉堡","沙拉","披萨","意大利面","鸡腿便当","刀削面","火锅","牛肉面","关东煮"]
+    @State var selectedFood: String?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:20) {
+            Image("dinner")
+                .resizable()
+                .scaledToFit()
+            
+            Text("今天吃什么？")
+                .fontWeight(.bold)
+            
+            if selectedFood != nil {
+                Text(selectedFood ?? "")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.accentColor)
+            }
+
+            Button{
+                selectedFood = food.shuffled().first
+            } label: {
+                Text("今天吃什么?")
+                    .frame(width: 200)
+            }
+            .buttonStyle(.borderedProminent)
+                     
+            Button{
+                selectedFood = nil
+            }label: {
+                Text("重置").frame(width: 200)
+            }
+
         }
         .padding()
+        .frame(maxHeight: .infinity)
+        .background(Color(.secondarySystemBackground))
+        .font(.title)
+        .buttonStyle(.bordered)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
+
+        .animation(.easeInOut,value: selectedFood)
     }
 }
 
