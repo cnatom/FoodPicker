b44413129f314f39be4ec45e2f3fa0338781ec0f
CUMT-Atom <cnatom@foxmail.com>
2023-10-05 19:15:08 +0800

    2_1_排版练习

--- FoodPicker/ContentView.swift
+++ FoodPicker/ContentView.swift
@@ -8,32 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    let food = ["汉堡","沙拉","披萨","意大利面","鸡腿便当","刀削面","火锅","牛肉面","关东煮"]
-    @State var selectedFood: String?
+    @State var selectedFood: Food?
+
     var body: some View {
-        VStack(spacing:20) {
-            Image("dinner")
-                .resizable()
-                .scaledToFit()
-            
+        VStack(spacing: 20) {
+            Spacer()
+            Group{
+                if selectedFood != nil {
+                    Text(selectedFood!.image)
+                        .font(.system(size: 250))
+                        .minimumScaleFactor(0.5) // 放不下的时候缩放倍数
+                        .lineLimit(1)
+                        .id(selectedFood!.name)
+                } else {
+                    Image("dinner")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            .frame(maxHeight: 250)
+
             Text("今天吃什么？")
                 .fontWeight(.bold)
             
             if selectedFood != nil {
-                Text(selectedFood ?? "")
+                Text(selectedFood?.name ?? "")
                     .font(.largeTitle)
                     .bold()
                     .foregroundColor(.accentColor)
-                    .id(selectedFood)
+                    .id(selectedFood?.name)
                     .transition(.asymmetric(insertion: .opacity.animation(.easeInOut), removal: .opacity.animation(.easeInOut)))
 //                    .transition(.slide.combined(with: .scale))
             }
+            
+            Spacer()
 
-            Button{
+            Button {
 //                withAnimation {
 //                    selectedFood = food.shuffled().first{$0 != selectedFood}
 //                }
-                selectedFood = food.shuffled().first{$0 != selectedFood}
+                selectedFood = Food.examples.shuffled().first { $0 != selectedFood }
             } label: {
                 Text(selectedFood == nil ? "告诉我" : "换一个")
                     .frame(width: 200)
@@ -41,23 +55,24 @@ struct ContentView: View {
             }
             .buttonStyle(.borderedProminent)
 
-            Button{
+            Button {
                 selectedFood = nil
-            }label: {
+            } label: {
                 Text("重置").frame(width: 200)
             }
-
         }
         .padding()
-        .frame(maxHeight: .infinity)
+        .frame(maxWidth: .infinity,maxHeight: .infinity)
         .background(Color(.secondarySystemBackground))
         .font(.title)
         .buttonStyle(.bordered)
         .buttonBorderShape(.capsule)
         .controlSize(.large)
-        .animation(.easeInOut,value: selectedFood)
+        .animation(.easeInOut, value: selectedFood)
     }
 }
+
+
 
 #Preview {
     ContentView()
