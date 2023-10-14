8bfe3669199f5080f70d7649ac171235aae71dd1
CUMT-Atom <cnatom@foxmail.com>
2023-10-14 17:20:06 +0800

    6_5_Start

---
+++ NetworkManagerPractice/AppEntry.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManagerPracticeApp.swift
+//  NetworkManagerPractice
+//
+//  Created by atom on 2023/10/14.
+//
+
+import SwiftUI
+
+// TODO: helll
+@main
+struct AppEntry: App {
+    init(){
+        applyTabBarBackground()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            HomeScreen()
+        }
+    }
+}
+
+
+extension AppEntry {
+    func applyTabBarBackground(){
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithTransparentBackground()
+        tabBarAppearance.backgroundColor = .systemBackground.withAlphaComponent(0.3)
+        tabBarAppearance.backgroundEffect = UIBlurEffect(style: .systemChromeMaterial)
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+    }
+}
